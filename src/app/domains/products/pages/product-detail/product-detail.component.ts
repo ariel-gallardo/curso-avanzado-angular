4800139aa9705014f62804cc41aee3e93bd386d0
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-05-10 20:45:55 -0300

    16-meta-tags# add title and description

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,19 +1,24 @@
-import { Component, inject, signal, OnInit, input, linkedSignal } from '@angular/core';
+import { Component, inject, signal, OnInit, input, linkedSignal, effect } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
+import { Meta, Title } from '@angular/platform-browser';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-detail',
   imports: [CommonModule, NgOptimizedImage],
   templateUrl: './product-detail.component.html',
 })
-export default class ProductDetailComponent implements OnInit {
+export default class ProductDetailComponent{
   readonly id = input<string>();
-  $product = signal<Product | null>(null);
+  $product = rxResource({
+    request: () => this.id(),
+    loader: ({request}) => this.productService.getOne(request)
+  });
   $cover = linkedSignal({
-    source: this.$product,
+    source: this.$product.value,
     computation: (p, oP) => {
       if (p) {
         const image = p.images[0];
@@ -21,30 +26,47 @@ export default class ProductDetailComponent implements OnInit {
         return '';
       }
       return '';
-    }
+    },
   });
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
-  ngOnInit() {
-    const id = this.id();
-    if (id) {
-      this.productService.getOne(id).subscribe({
-        next: (product) => {
-          this.$product.set(product);
-        },
-      });
-    }
-  }
+  private titleService = inject(Title);
+  private metaService = inject(Meta);
 
   changeCover(newImg: string) {
     this.$cover.set(newImg);
   }
 
   addToCart() {
-    const product = this.$product();
+    const product = this.$product.value();
     if (product) {
       this.cartService.addToCart(product);
     }
+  }
+
+  constructor(){
+    effect(() => {
+      if(this.$product.hasValue()){
+        const p = this.$product.value();
+        this.titleService.setTitle(p.title);
+        this.metaService.updateTag({
+          name: 'description',
+          content: p.description
+        });
+        this.metaService.updateTag({
+          name: 'og:title',
+          content: p.title,
+        });
+        this.metaService.updateTag({
+          name: 'og:description',
+          content: p.description,
+        });
+        this.metaService.updateTag({
+          name: 'og:image',
+          content: p.images && p.images.length > 0 ? p.images[0] : ''
+        })
+      }
+    });
   }
 }
