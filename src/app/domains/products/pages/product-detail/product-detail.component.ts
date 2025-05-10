3ba5a25c4891526940a5698037d753d4756a8fb0
Ariel Gallardo <38444032+ariel-gallardo@users.noreply.github.com>
2025-05-10 16:06:58 -0300

    Linked Signal on Product Detail (#13)
    * 11-linked-signal# Change cover with LinkedSignal
    
    * 11-linked-signal# cover as @let
    
    * 11-linked-signal# good practices

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnInit, input } from '@angular/core';
+import { Component, inject, signal, OnInit, input, linkedSignal } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
@@ -11,8 +11,18 @@ import { CartService } from '@shared/services/cart.service';
 })
 export default class ProductDetailComponent implements OnInit {
   readonly id = input<string>();
-  product = signal<Product | null>(null);
-  cover = signal('');
+  $product = signal<Product | null>(null);
+  $cover = linkedSignal({
+    source: this.$product,
+    computation: (p, oP) => {
+      if (p) {
+        const image = p.images[0];
+        if (image) return image;
+        return '';
+      }
+      return '';
+    }
+  });
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
@@ -21,21 +31,18 @@ export default class ProductDetailComponent implements OnInit {
     if (id) {
       this.productService.getOne(id).subscribe({
         next: (product) => {
-          this.product.set(product);
-          if (product.images.length > 0) {
-            this.cover.set(product.images[0]);
-          }
+          this.$product.set(product);
         },
       });
     }
   }
 
   changeCover(newImg: string) {
-    this.cover.set(newImg);
+    this.$cover.set(newImg);
   }
 
   addToCart() {
-    const product = this.product();
+    const product = this.$product();
     if (product) {
       this.cartService.addToCart(product);
     }
