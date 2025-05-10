5e2075d4611171bb7fd06217be92c2b33e769a8c
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-05-10 00:28:09 -0300

    03-environments# Add Path Alias

--- src/app/domains/shared/services/category.service.ts
+++ src/app/domains/shared/services/category.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Category } from '@shared/models/category.model';
-import { environment } from 'src/environments/environment';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
