2d7875c7effe155cf27060c51b09696d1b459fb6
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-05-10 19:29:36 -0300

    14-ssr# Counter ssr & fix childs about c

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -6,6 +6,7 @@ import {
   AfterViewInit,
   OnDestroy,
   model,
+  afterNextRender,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -14,47 +15,34 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './counter.component.html',
 })
-export class CounterComponent implements OnInit, AfterViewInit, OnDestroy {
+export class CounterComponent implements OnInit, OnDestroy {
   duration = input.required<number>();
   message = model.required<string>();
   counter = signal(0);
   counterRef: number | undefined;
 
   constructor() {
-    // NO ASYNC
-    // before render
-    // una vez
-    console.log('constructor');
-    console.log('-'.repeat(10));
+    afterNextRender(() => {
+      this.counterRef = window.setInterval(() => {
+        console.log('run interval');
+        this.counter.update((statePrev) => statePrev + 1);
+      }, 1000);
+      // after render
+      // hijos ya fueron pintandos
+      console.log('ngAfterViewInit');
+      console.log('-'.repeat(10));
+    })
   }
 
   ngOnInit() {
-    // after render
-    // una vez
-    // async, then, subs
-    console.log('ngOnInit');
-    console.log('-'.repeat(10));
-    console.log('duration =>', this.duration);
-    console.log('message =>', this.message);
-    this.counterRef = window.setInterval(() => {
-      console.log('run interval');
-      this.counter.update((statePrev) => statePrev + 1);
-    }, 1000);
-  }
-
-  ngAfterViewInit() {
-    // after render
-    // hijos ya fueron pintandos
-    console.log('ngAfterViewInit');
-    console.log('-'.repeat(10));
   }
 
   ngOnDestroy() {
     console.log('ngOnDestroy');
     console.log('-'.repeat(10));
-    window.clearInterval(this.counterRef);
+    if(this.counterRef) window.clearInterval(this.counterRef);
   }
-
+ 
   doSomething() {
     console.log('change duration');
     // async
