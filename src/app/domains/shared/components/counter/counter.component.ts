cf5c3346e5195f217d7dad47196b4c8a5fc48170
Ariel Gallardo <38444032+ariel-gallardo@users.noreply.github.com>
2025-05-10 15:34:38 -0300

    Migrate input to signals (#9)
    * 01-eslint# Remove unused imports & install eslint (#1)
    
    * Prettier & Eslint Integration (#2)
    
    * 02-prettier# Install prettier && script format
    
    * 02-prettier# AutoFormat with prettier
    
    * 02-prettier# Prettier & Eslint CFG
    
    * Environments & Alias (#3)
    
    * 03-environments# Generate Environments
    
    * 03-environments# Add Api Url
    
    * 03-environments# Fix Prettier & Url use env
    
    * 03-environments# Add Fake API Docs
    
    * 03-environments# Add Path Alias
    
    * Staging Configuration (#4)
    
    * 04-staging# fix format && stagging env file
    
    * 04-staging# Add build|serve on package
    
    * Good Practices (#5)
    
    * 05-good-practices# signal to let
    
    * 05-good-practices# using template if
    
    * Optimize Img Loading (#6)
    
    * 06-optimize-img# fix html crlf
    
    * 06-optimize-img# fix staging
    
    * 06-optimize-img# Product Detail
    
    * 06-optimize-img# Product Loading
    
    * 07-friendly-routes# Categories friendly robots | @let use (#7)
    
    * 08-signals# Communication between range and message (#8)
    
    * 09-input-to-signal# with command

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,14 +1,25 @@
-import { Component, Input, SimpleChanges, signal } from '@angular/core';
+import {
+  Component,
+  input,
+  SimpleChanges,
+  signal,
+  OnChanges,
+  OnInit,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-counter',
-    imports: [CommonModule],
-    templateUrl: './counter.component.html'
+  selector: 'app-counter',
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
 })
-export class CounterComponent {
-  @Input({required: true}) duration = 0;
-  @Input({required: true}) message = '';
+export class CounterComponent
+  implements  OnInit, AfterViewInit, OnDestroy
+{
+  duration = input.required<number>();
+  message = input.required<string>();
   counter = signal(0);
   counterRef: number | undefined;
 
@@ -20,17 +31,6 @@ export class CounterComponent {
     console.log('-'.repeat(10));
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    // before and during render
-    console.log('ngOnChanges');
-    console.log('-'.repeat(10));
-    console.log(changes);
-    const duration = changes['duration'];
-    if (duration && duration.currentValue !== duration.previousValue) {
-      this.doSomething();
-    }
-  }
-
   ngOnInit() {
     // after render
     // una vez
@@ -40,9 +40,9 @@ export class CounterComponent {
     console.log('duration =>', this.duration);
     console.log('message =>', this.message);
     this.counterRef = window.setInterval(() => {
-      console.log('run interval')
-      this.counter.update(statePrev => statePrev + 1);
-    }, 1000)
+      console.log('run interval');
+      this.counter.update((statePrev) => statePrev + 1);
+    }, 1000);
   }
 
   ngAfterViewInit() {
@@ -52,15 +52,14 @@ export class CounterComponent {
     console.log('-'.repeat(10));
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     console.log('ngOnDestroy');
     console.log('-'.repeat(10));
-    window.clearInterval(this.counterRef)
+    window.clearInterval(this.counterRef);
   }
 
   doSomething() {
-    console.log('change duration')
+    console.log('change duration');
     // async
   }
-
 }
